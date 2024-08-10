72d1f08050364a8b80f38a2cda4f519040006b63
Mohammed Sinan <sinanksinu0@gmail.com>
2024-08-09 21:29:11 -0700

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -48,18 +48,18 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      return next(errorHandler(404, 'User not found'));
+     return next(errorHandler(404, 'User not found'));
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
       return next(errorHandler(400, 'Invalid password'));
     }
     const token = jwt.sign(
-      { id: validUser._id, isAdmin: validUser.isAdmin },
+      { id: validUser._id },
       process.env.JWT_SECRET
     );
 
-    const { password: pass, ...rest } = validUser._doc;
+      const { password: pass, ...rest } = validUser._doc;
 
     res
       .status(200)
