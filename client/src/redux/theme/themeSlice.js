f14b5639eba2fd07d45e1925b16bc578e0787e83
Mohammed Sinan <sinanksinu0@gmail.com>
2024-08-14 20:47:19 -0700

    complete dark mode functionality

--- client/src/redux/theme/themeSlice.js
+++ client/src/redux/theme/themeSlice.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import {createSlice} from '@reduxjs/toolkit';
 
-export default function themeSlice() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const initialState = {
+    theme: 'light',
+};
+
+const themeSlice = createSlice({
+    name: 'theme',
+    initialState,
+    reducers: {
+        toggleTheme: (state) => {
+            state.theme = state.theme === 'light' ? 'dark' : 'light';
+        },
+        }
+});
+
+export const {toggleTheme} = themeSlice.actions;
+
+export default themeSlice.reducer;
